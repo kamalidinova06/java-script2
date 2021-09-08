7918f792ae473cd32f812a9124b19b48a17d41e5
kamaldina <tattybubunevis@gmal.com>
2021-09-08 09:26:08 +0600

    initial

---
+++ script.js
@@ -0,0 +1,47 @@
+//
+//
+// console.log(typeof num)
+//
+// let a = 4.98
+//
+// console.log( Math.ceil(a) ) // 5
+// console.log( Math.round(a) ) //5
+// console.log( Math.floor(a) )//4
+// console.log( Math.pow(a,3) )  //4.98 * 4.98 * 4.98
+// console.log( a**3 ) //4.98 * 4.98 * 4.98
+// let bool = false
+// console.log(typeof bool)
+//
+// let  age  = 25
+//
+// if (age > 21 &&  age>90) {
+//     console.log("Welcome")
+// }else {
+// console.log("Goodbye")
+// }
+//
+// // && logicheskoe E
+// // || logicheskoe ELE
+//
+// console.log( true || false)
+//
+//
+
+let a = 6
+
+let b = a + 3
+console.log(b)
+
+let c = "Taty"
+let d = "Nazi"
+console.log(c + d) // TatyNazi
+console.log(c + " " + d) // Taty Nazi
+
+
+let t = "7"
+let l = "6"
+console.log(t+l) // "76"
+console.log(typeof +t) // number
+console.log(typeof l) // string
+console.log(+t + +l) //13
+
